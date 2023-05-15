c1648c87a1bff067b60e77d86b25bb00e95a20ee
ShuvoMallick01 <shuvacse01@gmail.com>
2023-05-15 18:34:46 +0600

    Adding Javascript for Slider

--- js/app.js
+++ js/app.js
@@ -1,3 +1,48 @@
-$(function () {
-  $("#datepicker").datepicker();
+// $(function () {
+//   $("#datepicker").datepicker();
+// });
+
+// === SLIDER USING SWPER JS PLUGIN ===
+// init Swiper:
+const swiper = new Swiper(".swiper", {
+  // Default parameters
+  slidesPerView: 3,
+  spaceBetween: 18,
+
+  // // If we need pagination
+  // pagination: {
+  //   el: ".swiper-pagination",
+  // },
+
+  // // Navigation arrows
+  // navigation: {
+  //   nextEl: ".swiper-button-next",
+  //   prevEl: ".swiper-button-prev",
+  // },
+
+  // // And if we need scrollbar
+  // scrollbar: {
+  //   el: ".swiper-scrollbar",
+  // },
+
+  // // Responsive breakpoints
+  // breakpoints: {
+  //   // when window width is >= 320px
+  //   320: {
+  //     slidesPerView: 2,
+  //     spaceBetween: 20,
+  //   },
+  //   // when window width is >= 480px
+  //   480: {
+  //     slidesPerView: 3,
+  //     spaceBetween: 30,
+  //   },
+  //   // when window width is >= 640px
+  //   640: {
+  //     slidesPerView: 4,
+  //     spaceBetween: 40,
+  //   },
+  // },
 });
+
+// === /SLIDER USING SWPER JS PLUGIN ===
