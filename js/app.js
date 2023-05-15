6565325b5c821782d11968d7f8fcd993e82c684f
ShuvoMallick01 <shuvacse01@gmail.com>
2023-05-15 23:00:59 +0600

    Adding Javascript for Slider

--- js/app.js
+++ js/app.js
@@ -5,42 +5,38 @@
 // === SLIDER USING SWPER JS PLUGIN ===
 // init Swiper:
 const swiper = new Swiper(".swiper", {
+  // effect: slideEffect || "slide",
+
   // Default parameters
   slidesPerView: 3,
   spaceBetween: 18,
 
-  // // If we need pagination
-  // pagination: {
-  //   el: ".swiper-pagination",
-  // },
-
   // // Navigation arrows
-  // navigation: {
-  //   nextEl: ".swiper-button-next",
-  //   prevEl: ".swiper-button-prev",
-  // },
-
-  // // And if we need scrollbar
-  // scrollbar: {
-  //   el: ".swiper-scrollbar",
-  // },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 
   // // Responsive breakpoints
+  // Responsive
   // breakpoints: {
-  //   // when window width is >= 320px
-  //   320: {
+  //   0: {
+  //     slidesPerView: 1,
+  //     spaceBetween: 20,
+  //   },
+
+  //   992: {
   //     slidesPerView: 2,
   //     spaceBetween: 20,
   //   },
-  //   // when window width is >= 480px
-  //   480: {
+
+  //   1200: {
   //     slidesPerView: 3,
-  //     spaceBetween: 30,
+  //     spaceBetween: 20,
   //   },
-  //   // when window width is >= 640px
-  //   640: {
-  //     slidesPerView: 4,
-  //     spaceBetween: 40,
+  //   1400: {
+  //     slidesPerView: slidePerView ? Number(slidePerView) : 4,
+  //     spaceBetween: 20,
   //   },
   // },
 });
