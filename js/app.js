c1da584333c122ed1331aa1282b3441ba2b4194c
ShuvoMallick01 <shuvacse01@gmail.com>
2023-05-19 21:12:25 +0600

    Fixing Range JS Plugin noUiSlider Code

--- js/app.js
+++ js/app.js
@@ -86,15 +86,15 @@ const input1 = document.getElementById("input-with-keypress-1");
 const inputs = [input0, input1];
 
 noUiSlider.create(stepsSlider, {
-  start: [20, 80],
+  start: [50, 500],
   connect: true,
   tooltips: [true, wNumb({ decimals: 1 })],
   range: {
     min: [0],
-    "10%": [10, 10],
-    "50%": [80, 50],
-    "80%": 150,
-    max: 200,
+    // "10%": [10, 100],
+    // "50%": [100, 500],
+    // "80%": 800,
+    max: 1000,
   },
 });
 
